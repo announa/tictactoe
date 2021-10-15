2b86941f118b57d75e610e81197ac0d1dee99dc0
chirimoia <moupou@posteo.org>
2021-10-15 14:24:32 -0500

    responsive

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+let currentPlayer = 'Player 1';
 let selectedFields = [];
+let fieldsPlayer1 = [];
+let fieldsPlayer2 = [];
+let counter = 0;
 let circle = document.getElementsByClassName('circle');
 let cross = document.getElementsByClassName('cross');
 let fields = document.getElementsByTagName('td');
-let currentPlayer = 'cross';
-let fieldsPlayer1 = [];
-let fieldsPlayer2 = [];
+let checkIfWinner = ['', []];
 let winningCombinations = [
   [0, 1, 2],
   [3, 4, 5],
@@ -15,138 +17,144 @@ let winningCombinations = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-let checkIfWinner;
 
 function selectField(id) {
   if (!selectedFields[id]) {
-    selectedFields[id] = currentPlayer;
-    doMove(id);
-    changePlayerPanel();
+    setVariables(id);
+    renderFieldSelection(id);
     checkForWin();
-    checkIfGameOver();
     changeCurrentPlayer();
   }
 }
 
-/* renders the current players field selection and sets the checkIfWinner-variable for later checkForWin */
-function doMove(id) {
-  if (currentPlayer == 'cross') {
-    cross[id].classList.remove('d-none');
+function setVariables(id) {
+  selectedFields[id] = currentPlayer;
+  counter++;
+  if (currentPlayer == 'Player 1') {
     fieldsPlayer1.push(id);
-    checkIfWinner = fieldsPlayer1;
+    checkIfWinner[0] = currentPlayer;
+    checkIfWinner[1] = fieldsPlayer1;
   } else {
-    circle[id].classList.remove('d-none');
     fieldsPlayer2.push(id);
-    checkIfWinner = fieldsPlayer2;
+    checkIfWinner[0] = currentPlayer;
+    checkIfWinner[1] = fieldsPlayer2;
   }
-  fields[id].classList.remove('hover');
 }
 
-/* shows the current player on the boards player panel */
-function changePlayerPanel() {
-  if (currentPlayer == 'cross') {
-    document.getElementById('player-1').classList.remove('inactive');
-    document.getElementById('player-2').classList.add('inactive');
+/* renders the current players field selection and sets the checkIfWinner-variable for later checkForWin */
+function renderFieldSelection(id) {
+  fields[id].classList.remove('hover');
+  if (currentPlayer == 'Player 1') {
+    cross[id].classList.remove('d-none');
   } else {
-    document.getElementById('player-1').classList.add('inactive');
-    document.getElementById('player-2').classList.remove('inactive');
+    circle[id].classList.remove('d-none');
   }
 }
 
 function checkForWin() {
   if (hasFullRow() > -1) {
     let indexOfWinningCombination = hasFullRow();
-    renderWinningLine(indexOfWinningCombination);
+    renderWinnerLine(indexOfWinningCombination);
     /* gameOver(); */
     setTimeout(() => {
       gameOver('win');
     }, 400);
+  } else {
+    checkIfGameOver();
   }
 }
 
 /* checks if the current player has a full row ( = winningCombination)
-** returns the index of the winning combination in the variable winningCombination */
+ ** returns the index of the winning combination in the variable winningCombination */
 function hasFullRow() {
   let indexOfWinningCombination = winningCombinations.findIndex((combination) => playerHasFullCombination(combination));
   return indexOfWinningCombination;
 }
 
 /* checks if the selected fields of the current player ( = checkIfWinner) contain all the numbers of the winningCombination
-** returns true or false */
-function playerHasFullCombination(combination){
-  return combination.every((number) => checkIfWinner.includes(number));
+ ** returns true or false */
+function playerHasFullCombination(combination) {
+  return combination.every((number) => checkIfWinner[1].includes(number));
 }
 
-function highlightFields(id) {
-  fields[id].classList.add('win');
-}
-
-function renderWinningLine(index){
-  let line = document.getElementById('winning-line');
-  line.classList.remove('winning-line-0');
-  if(index <=2){
-    line.style.top = 'calc(50% + ((100% + 2px) *' + (index)+ '))';
-    line.style.left = 'calc(12.5% + 3px)';
-    line.style.transform = 'translateY(-50%)';
-  }if(index >= 3 && index <= 5){
-    line.style.transform = 'rotate(90deg)';
-    line.style.top = 'calc(12.5% - 3px)';
-    line.style.left = 'calc(50% + (100% + 2px) *' + (index - 3)+ ')'
-  }if(index == 6){
-    line.style.top = '30%'
-    line.style.left = '30%'
-    line.style.width = '350%';
-    line.style.transform = 'translateY(-5px) rotate(45deg)';
-  }if(index == 7){
-    line.style.top = '270%'
-    line.style.left = '30%'
-    line.style.width = '350%';
-    line.style.transform = 'rotate(-45deg)';
-  }
-}
-
-function changeCurrentPlayer() {
-  if (currentPlayer == 'cross') {
-    currentPlayer = 'circle';
-  } else {
-    currentPlayer = 'cross';
+function checkIfGameOver() {
+  if (counter == 9) {
+    gameOver('nowin');
   }
 }
 
 function gameOver(status) {
   document.getElementById('game-over').classList.remove('minimize');
-  if(status === 'win'){
-  document.getElementById('game-over-text').innerHTML = `${currentPlayer} wins!`;
-} else{
-  document.getElementById('game-over-text').innerHTML = `Game Over!`;
+  if (status === 'win') {
+    document.getElementById('game-over-text').innerHTML = `${checkIfWinner[0]} wins!`;
+  } else {
+    document.getElementById('game-over-text').innerHTML = `Game Over!`;
   }
 }
 
-function checkIfGameOver(){
-  if(selectedFields.length == 9){
-    gameOver('nowin');
+
+/* changes the current player and shows the current player on the boards player panel */
+function changeCurrentPlayer() {
+  if (currentPlayer == 'Player 1') {
+    currentPlayer = 'Player 2';
+    document.getElementById('player-1').classList.add('inactive');
+    document.getElementById('player-2').classList.remove('inactive');
+  } else {
+    currentPlayer = 'Player 1';
+    document.getElementById('player-1').classList.remove('inactive');
+    document.getElementById('player-2').classList.add('inactive');
   }
 }
 
-function replay() {
-  document.getElementById('winning-line').classList.add('winning-line-0');
+function restart() {
+  document.getElementById('winner-line').classList.add('winner-line-0');
   document.getElementById('game-over').classList.add('minimize');
   resetVariables();
-  resetFields();
+  resetBoard();
 }
 
-function resetVariables(){
+function resetVariables() {
   selectedFields = [];
   fieldsPlayer1 = [];
   fieldsPlayer2 = [];
-  currentPlayer = 'cross';
+  currentPlayer = 'Player 1';
+  counter = 0;
 }
 
-function resetFields() {
+function resetBoard() {
   for (i = 0; i < 9; i++) {
     circle[i].classList.add('d-none');
     cross[i].classList.add('d-none');
     fields[i].classList.add('hover');
+  }
+  document.getElementById('player-1').classList.remove('inactive');
+  document.getElementById('player-2').classList.add('inactive');
+}
+
+function renderWinnerLine(index) {
+  let line = document.getElementById('winner-line');
+  line.classList.remove('winner-line-0');
+  if (index <= 2) {
+    line.style.top = 'calc(50% + ((100% + 2px) *' + index + '))';
+    line.style.left = 'calc(12.5% + 3px)';
+    line.style.transform = 'translateY(-50%)';
+  }
+  if (index >= 3 && index <= 5) {
+    line.style.transform = 'rotate(90deg)';
+    line.style.top = 'calc(12.5% - 3px)';
+    line.style.left = 'calc(50% + (100% + 2px) *' + (index - 3) + ')';
+  }
+  if (index == 6) {
+    line.style.top = '30%';
+    line.style.left = '30%';
+    line.style.width = '350%';
+    line.style.transform = 'translateY(-5px) rotate(45deg)';
+  }
+  if (index == 7) {
+    line.style.top = '270%';
+    line.style.left = '30%';
+    line.style.width = '350%';
+    line.style.transform = 'rotate(-45deg)';
   }
 }
 
